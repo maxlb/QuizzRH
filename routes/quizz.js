80ef280d5aef3234521dd5347ef14f6a77497b50
Maxime LE BAIL <maxime.lebail@orange.fr>
2019-11-26 11:28:52 +0100

    suppr com

--- routes/quizz.js
+++ routes/quizz.js
@@ -19,25 +19,4 @@ router.get('/:id', async function(req, res, next) {
           .catch(jsonKO => { res.json(jsonKO) });
 });
 
-/*
-router.post('/answered/:id', async function(req, res){ 
-  // Autoriser l'accès
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods','POST');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  if (req.body) {
-		console.log(`Server - Infos reçues : ${req.body}`);
-    var sql = `SELECT * FROM quizz WHERE idquizz = ${req.params.id}`;
-    /*
-    utils.post(res,sql);
-		// Appel et réponse de la statistique
-		await stats.getStat(req.params.stat, req.body.token, req.body.orga, req.body.login)
-						.then(obj => { res.json( { ok:true , data:obj } )} )
-						.catch(err => res.json( { ok:false , error:err } ));
-	} else {
-		res.json( utils.getJSONResponse('Aucune données reçues...',null) );
-  }
-});*/
-
 module.exports = router;
