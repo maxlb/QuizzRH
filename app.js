89685604189207203267483bcf07f8debcdf0a7e
Maxime LE BAIL <maxime.lebail@orange.fr>
2019-11-27 13:42:20 +0100

    chatbot post

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 const Database = require('./db');
 
 /*** Initialisation de l'application ***/
 var app = express();
 app.use(logger('dev'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 /*** Connexion à la base de donnée ***/
 app.use(function(req, res, next){
@@ -29,14 +33,30 @@ var indexRouter = require('./routes/index');
 var quizzRouter = require('./routes/quizz');
 var userRouter  = require('./routes/user');
 var rhRouter    = require('./routes/rh');
-var chatRouter    = require('./routes/chatbot');
+var chatRouter  = require('./routes/chatbot');
 
 /*** Routeur ***/
+app.use('/api-quizz/chatbot', 	chatRouter);
 app.use('/'					, 	indexRouter);
 app.use('/api-quizz'		, 	indexRouter);
 app.use('/api-quizz/quizz'	, 	quizzRouter);
 app.use('/api-quizz/user'	, 	userRouter);
 app.use('/api-quizz/rh'		, 	rhRouter);
-app.use('/api-quizz/chatbot', 	chatRouter);
 
+/*
+app.post('/api-quizz/chatbot', (req, res) => {
+    console.log('ok');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    
+    console.log(req.body);
+
+    res.json({
+        speech: "Merci pour ces informations",
+        displayText: "Merci pour ces informations",
+        source: 'Système SummitRH'
+    });
+});
+*/
 module.exports = app;
--- routes/chatbot.js
+++ routes/chatbot.js
@@ -3,19 +3,22 @@ var utils = require('../utils');
 
 var router = express.Router();
 
-router.post('/chatbot', (req, res) => {
+router.post('/chatbot', function(req, res) {
+    console.log('ok');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     
     console.log(req.body);
-    console.log(req.body.result.parameters["rating"]);
-    console.log(req.body.result.parameters["comments"]);
-    console.log(req.body.result.parameters["resort-location"]);
+    //console.log(req.body.result.parameters["rating"]);
+    //console.log(req.body.result.parameters["comments"]);
+    //console.log(req.body.result.parameters["resort-location"]);
 
-    res.status(200).json({
+    res.json({
         speech: "Merci pour ces informations",
         displayText: "Merci pour ces informations",
-        source: 'Système SummitRH'});
+        source: 'Système SummitRH'
+    });
 });
 
-   module.exports = router;
+module.exports = router;
